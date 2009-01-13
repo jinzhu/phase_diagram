a6448457f315e6c798c76e222b432c5911fda1ec
wosmvp <wosmvp@gmail.com>
2009-01-13 16:52:11 +0800

    Fix show sidebar

--- pd.rb
+++ pd.rb
@@ -3,7 +3,7 @@ class Pd
     @path      = File.join( CONFIG_PATH , args )
     @config    = YAML.load_file(File.join( @path ,'config'))
     Dir.chdir(@path)
-    @table     = File.exist?('element.csv') ? Table('element.csv') : []
+    @table     = File.exist?('element.csv') ? Table('element.csv') : Table('')
     @elements  = @config.keys
   end
 
@@ -19,21 +19,35 @@ class Pd
     show_sidebar
   end
 
+  def show_sidebar
+    $sidebar.content do
+      t = @elements + @table.column(@table.column_names[0])
+
+      @it = []
+      t.size.times do |x|
+        $app.para "\n"
+        $app.para $app.strong(t[x]) ,:stroke => "#f00"
+        @it[x] = $app.edit_line :width => 100,:left => 100
+      end
+    end
+  end
+
   def show_element
     $content.content do
-      n,t,d = [],[],[]
-
+      # 表格标题
       @config.keys.each_with_index do |x,y|
-        $app.edit_line '', :width => 150,:state => 'disabled' if y == 0
-        n[y] = $app.edit_line :text => x, :width => 150,:state => 'disabled'
+        $app.edit_line '摩尔替换率', :width => 150,:state => 'disabled' if y == 0
+        $app.edit_line :text => x, :width => 150,:state => 'disabled'
       end
 
+      # 表格内容，最后加入删除按钮,删除需要删除行内容，以及该行
+      t,d = [],[]
       items = $content.flow do
         @table.size.times do |x|
           t[x]=[]
           d[x] = $content.flow do
             (@config.keys.size + 1).times do |y|
-              t[x][y] = $content.edit_line @table[x] ? @table[x][y] : '' , :width => 150
+              t[x][y] = $content.edit_line  @table[x][y], :width => 150
             end
             $content.button "删除",:width => 100 do d[x].remove && t[x]=[] end
           end
@@ -41,6 +55,7 @@ class Pd
       end
       $app.para "\n"
 
+      # 空白一行，用来向表单中添加内容
       tt    = []
       (@config.keys.size + 1).times do |y|
         tt[y] = $content.edit_line '' , :width => 150
@@ -52,7 +67,7 @@ class Pd
           t[size] = []
           d[size] = $content.flow do
             (@config.keys.size + 1).times do |x|
-              t[size][x] = edit_line tt[x].text, :width => 150
+              t[size][x] = $content.edit_line tt[x].text, :width => 150
             end
 
             $content.button "删除",:width => 100 do
@@ -63,26 +78,16 @@ class Pd
       end
       $content.para "\n"*2
 
+      # 保存内容为 csv 文件
       $content.button "保存",:width => 100 do
         File.open('element.csv','w+') do |x|
-          x << ',' + n.map(&:text).join(',') + "\n"
+          x << ',' + @config.keys.join(',') + "\n"
           t.map do |y|
             x << y.map(&:text).join(',') + "\n" unless y.empty?
           end
         end
-      end
-    end
-  end
-
-  def show_sidebar
-    $sidebar.content do
-      t = @elements.concat(@table.column(@table.column_names[0]))
-
-      @it = []
-      t.size.times do |x|
-        $app.para "\n"
-        $app.para $app.strong(t[x]) ,:stroke => "#f00"
-        @it[x] = $app.edit_line :width => 100,:left => 100
+        # 更新边
+        show_sidebar
       end
     end
   end
